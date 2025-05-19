77bffa445de47d62b394612fb9ca599946d1a855
aliyan jabbar <jabbaraliyan805@gmail.com>
2025-05-19 18:10:32 +0500

    adding classes

---
+++ class07/category/category.py
@@ -0,0 +1,11 @@
+# 1.
+# def get_category():
+#     return "here are categories"
+
+
+# 2.
+class Category:
+    def __init__(self):
+        pass
+    def get_category(self):
+        return "here are categories"
